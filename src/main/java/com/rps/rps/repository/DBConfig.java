1f80cac241afd165f177b57216c0ff771ee0139a
glaschke <s.glaschke@gmx.net>
2023-02-16 09:54:34 +0100

    docker config for mongodb container

--- src/main/java/com/rps/rps/repository/DBConfig.java
+++ src/main/java/com/rps/rps/repository/DBConfig.java
@@ -4,14 +4,16 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import lombok.RequiredArgsConstructor;
 import org.bson.Document;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DBConfig {
 
     public MongoCollection<Document> getDB(String collection){
-        MongoClient mongoClient = MongoClients.create();
+        MongoClient mongoClient = MongoClients.create("mongodb://" + "host.docker.internal" + ":27017");
 
         MongoDatabase DB = mongoClient.getDatabase("test");
         return DB.getCollection(collection);
--- src/test/java/com/rps/rps/services/GameControlServiceTest.java
+++ src/test/java/com/rps/rps/services/GameControlServiceTest.java
@@ -5,15 +5,12 @@ import com.rps.rps.gameitems.Result;
 import com.rps.rps.models.MatchModel;
 import com.rps.rps.models.PlayerModel;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.anyOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
